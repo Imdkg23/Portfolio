f7ec7e7230a4eb919032091c981a85c7f46d5fea
Deep Kumar Ghosh <dkg2153@gmail.com>
2026-09-12 16:36:52 +0530

    Updated project

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -19,6 +19,12 @@ const Work = () => {
       liveLink: "https://react-password-generator-olive-theta.vercel.app/",
       github: "https://github.com/Imdkg23/React-Password-Generator",
     },
+    {
+      title: "Skin Cancer Detection using CNN",
+      description: "Developed an ML model that classifies between two skin cancer lesions.",
+      image: cnn,
+      github: "https://github.com/Imdkg23/SkinCancer",
+    }
   ];
 
   return (
